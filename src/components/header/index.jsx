2311c7e5300647390994bf224c49f2badaa50184
afialydia <afia.caruso@gmail.com>
2022-01-06 20:02:40 -0500

    updated header ui,

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,119 +6,71 @@ import {
 	Heading,
 	chakra,
 	Box,
+	Stack,
 	Spacer,
 	useBreakpointValue,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import "./header.styles.scss";
 import { auth } from "../../firebase/firebase.utils";
-import { ReactComponent as Logo } from "../../assets/Logo.svg";
 
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import ShoppingCart from "../cart";
 
-import CartIcon from "../cart-icon";
-import CartDropdown from "../cart-dropdown";
-import { selectCartHidden } from "../../redux/cart/cart.selectors";
-import { selectCurrentUser } from "../../redux/user/user.selectors";
-
-import { signOutStart } from "../../redux/user/user.actions";
-
-const Header = ({ currentUser, hidden, signOutStart }) => {
-	const MotionFlex = motion(Flex, { forwardMotionProps: true });
-
-	const [lastYPos, setLastYPos] = React.useState(0);
-	const [shouldShowActions, setShouldShowActions] = React.useState(true);
-
-	React.useEffect(() => {
-		function handleScroll() {
-			const yPos = window.scrollY;
-			const isScrollingUp = yPos < lastYPos;
-
-			setShouldShowActions(isScrollingUp);
-			setLastYPos(yPos);
-		}
-
-		window.addEventListener("scroll", handleScroll, false);
-
-		return () => {
-			window.removeEventListener("scroll", handleScroll, false);
-		};
-	}, [lastYPos]);
-
+const Header = () => {
+	
 	return (
 		<Flex
-			// h="100%"
+			h="auto"
 			w="100%"
 			justify="space-between"
 			// marginBottom="1.5rem"
-			className="header"
-			positon="fixed"
 			// border="solid yellow"
-			align="center"
-			// align="flex-end"
+			align="flex-start"
 			// border="solid red .25px"
 			position="fixed"
 			zIndex={6}
 			top="0"
-			paddingTop={useBreakpointValue({ base: 4, md: '1.6rem' })}
-			paddingRight={useBreakpointValue({ base: 4, md: '1.6rem' })}
+			paddingTop={useBreakpointValue({ base: 4, md: "1.6rem" })}
+			paddingX={useBreakpointValue({ base: 4, md: "1.6rem" })}
 		>
-			{/* <chakra.span h="100%" w="4.375rem" className="logo-container">
-			<Link to="/">
-				<Logo className="logo" />
-			</Link>
-		</chakra.span> */}
+			<Stack
+				// border="solid teal"
+				maxW={"2xl"}
+				align={"flex-start"}
+				spacing={6}
+				h="90%"
+			>
+				<Heading size={"4xl"}>
+					POCKET
+					<br />
+					FULL OF
+					<br />
+					POSIES
+				</Heading>
+			</Stack>
 			<Spacer />
 			<Flex
 				w="50%"
 				h="100%"
 				// align="center"
-				justify="flex-end"
+				justify="end"
+				align="start"
 				className="options"
 				// border="solid"
 			>
-				{" "}
-				<motion.div
-					zIndex={3}
-					initial={{ opacity: 1 }}
-					animate={{ opacity: shouldShowActions ? 1 : 0 }}
-					transition={{ opacity: { duration: 0.2 } }}
-				>
-					<Link className="option" to="/">
-						<Heading>HOME</Heading>
-					</Link>
-				</motion.div>
+				<Link className="option" to="/">
+					<Heading fontSize={{ base: "md", md: "4xl" }}>HOME</Heading>
+				</Link>
 				<Box w="1rem" />
-				<motion.div
-					zIndex={3}
-					initial={{ opacity: 1 }}
-					animate={{ opacity: shouldShowActions ? 1 : 0 }}
-					transition={{ opacity: { duration: 0.2 } }}
-				>
-					<Link className="option" to="/shop">
-						<Heading>SHOP</Heading>
-					</Link>
-				</motion.div>
+				<Link className="option" to="/shop">
+					<Heading fontSize={{ base: "md", md: "4xl" }}>SHOP</Heading>
+				</Link>
 				<Box w="1rem" />
-				<motion.div
-					zIndex={3}
-					initial={{ opacity: 1 }}
-					animate={{ opacity: shouldShowActions ? 1 : 0 }}
-					transition={{ opacity: { duration: 0.2 } }}
-				>
-					<Link className="option" to="/contact">
-						<Heading>CONTACT</Heading>
-					</Link>
-				</motion.div>
+				<Link className="option" to="/contact">
+					<Heading fontSize={{ base: "md", md: "4xl" }}>CONTACT</Heading>
+				</Link>
 				<Box w="1rem" />
-				<motion.div
-					zIndex={3}
-					initial={{ opacity: 1 }}
-					animate={{ opacity: shouldShowActions ? 1 : 0 }}
-					transition={{ opacity: { duration: 0.2 } }}
-				>
-					{currentUser ? (
+				{/* {currentUser ? (
 						<chakra.div className="option" onClick={() => signOutStart()}>
 							<Heading> SIGN OUT</Heading>
 						</chakra.div>
@@ -126,23 +78,12 @@ const Header = ({ currentUser, hidden, signOutStart }) => {
 						<Link className="option" to="/signin">
 							<Heading> SIGN IN</Heading>
 						</Link>
-					)}
-				</motion.div>
-				<Box w="1rem" />
-				<CartIcon />
+					)} */}
+				{/* <Box w="1rem" /> */}
+				<ShoppingCart />{" "}
 			</Flex>
-			{hidden ? null : <CartDropdown />}{" "}
 		</Flex>
 	);
 };
 
-const mapStateToProps = createStructuredSelector({
-	currentUser: selectCurrentUser,
-	hidden: selectCartHidden,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-	signOutStart: () => dispatch(signOutStart()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default Header;
